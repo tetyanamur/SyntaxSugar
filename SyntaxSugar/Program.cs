af139ebeb8bf8d41885a73ab7e0c8da1b5f44772
Tetyana Murzina <tetyanamur@gmail.com>
2022-08-01 03:01:51 -0500

    initial commit

--- SyntaxSugar/Program.cs
+++ SyntaxSugar/Program.cs
@@ -7,7 +7,7 @@ namespace SyntaxSugar
         static void Main(string[] args)
         {
             var ans = 4;
-            ;
+            
             var response= (ans < 9)? $"{ans} is less than nine": $"{ans} is greater than or equal to nine";
              
             Console.WriteLine(response);
